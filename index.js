45a205562a707e0e2158e6bb13f80ced1f6a294d
Irene <irenemanningm@gmail.com>
2021-09-13 14:43:59 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+var a, b
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(a) {
+    return (a + 1);
+}
+
+function decrement(a) {
+    return (a - 1);
+}
+
+var n
+
+function makeInt(n) {
+     n=parseInt(n, 10);
+     return n;
+}
+n ='2.222'
+function preserveDecimal(n) {
+   n=parseFloat(n);
+    return n;
+}
+
